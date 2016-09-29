6d23c91767ed30e6dae28f909aaca7b07c03c8b9
ngbaanh <nba.1.6.94@gmail.com>
2016-09-29 22:14:17 +0700

    + Join Table: + GUI Design
    Signed-off-by: ngbaanh <nba.1.6.94@gmail.com>

---
+++ CoffeeManagement/CoffeeManagement/Views/Popups/JoinTable.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using CoffeeManagement.Utilities;
+
+namespace CoffeeManagement.Views.Popups
+{
+    public partial class JoinTable : Form
+    {
+        public JoinTable()
+        {
+            InitializeComponent();
+        }
+
+        private void cancelBtn_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void okBtn_Click(object sender, EventArgs e)
+        {
+            if (MessageHelper.CreateYesNoQuestion("Bạn thực sự muốn gộp bàn?") == DialogResult.Yes)
+            {
+                // Do joining Table List, Bill Data.
+                MessageHelper.CreateMessage("Chức năng đang xây dựng");
+            }
+        }
+    }
+}
