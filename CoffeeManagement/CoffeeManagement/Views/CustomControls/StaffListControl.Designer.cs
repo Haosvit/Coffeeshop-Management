a40e3899bdb3b3b58515abf33d524165e7e86e4b
ngbaanh <nba.1.6.94@gmail.com>
2016-09-29 23:34:06 +0700

    + Custom Controls:
    + Staff List Control (the left)
    + Work Tracking Detail Control (the right top)
    + Work Tracking List Control (the right bottom)
    
    Signed-off-by: ngbaanh <nba.1.6.94@gmail.com>

---
+++ CoffeeManagement/CoffeeManagement/Views/CustomControls/StaffListControl.Designer.cs
@@ -0,0 +1,110 @@
+﻿namespace CoffeeManagement.Views.DetailViews
+{
+    partial class StaffListControl
+    {
+        /// <summary> 
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Component Designer generated code
+
+        /// <summary> 
+        /// Required method for Designer support - do not modify 
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.searchBox1 = new CoffeeManagement.Views.CustomControls.SearchBox();
+            this.flowLayoutPanel1 = new System.Windows.Forms.FlowLayoutPanel();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.flowLayoutPanel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(3, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(151, 16);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Danh sách nhân viên";
+            // 
+            // searchBox1
+            // 
+            this.searchBox1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.searchBox1.AutoSize = true;
+            this.searchBox1.BackColor = System.Drawing.Color.White;
+            this.searchBox1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.searchBox1.Location = new System.Drawing.Point(3, 19);
+            this.searchBox1.Name = "searchBox1";
+            this.searchBox1.SearchContent = "";
+            this.searchBox1.Size = new System.Drawing.Size(210, 26);
+            this.searchBox1.TabIndex = 1;
+            // 
+            // flowLayoutPanel1
+            // 
+            this.flowLayoutPanel1.Controls.Add(this.label1);
+            this.flowLayoutPanel1.Controls.Add(this.searchBox1);
+            this.flowLayoutPanel1.Controls.Add(this.listBox1);
+            this.flowLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.flowLayoutPanel1.FlowDirection = System.Windows.Forms.FlowDirection.TopDown;
+            this.flowLayoutPanel1.Location = new System.Drawing.Point(0, 0);
+            this.flowLayoutPanel1.Name = "flowLayoutPanel1";
+            this.flowLayoutPanel1.Size = new System.Drawing.Size(216, 409);
+            this.flowLayoutPanel1.TabIndex = 2;
+            // 
+            // listBox1
+            // 
+            this.listBox1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.listBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 16;
+            this.listBox1.Items.AddRange(new object[] {
+            "Nguyễn Ba Ten Đơ",
+            "Lê Pha Chế",
+            "Pha Ly"});
+            this.listBox1.Location = new System.Drawing.Point(3, 51);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(210, 341);
+            this.listBox1.TabIndex = 2;
+            // 
+            // StaffListControl
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.flowLayoutPanel1);
+            this.Name = "StaffListControl";
+            this.Size = new System.Drawing.Size(216, 409);
+            this.flowLayoutPanel1.ResumeLayout(false);
+            this.flowLayoutPanel1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private CustomControls.SearchBox searchBox1;
+        private System.Windows.Forms.FlowLayoutPanel flowLayoutPanel1;
+        private System.Windows.Forms.ListBox listBox1;
+    }
+}
---
+++ CoffeeManagement/CoffeeManagement/Views/CustomControls/WorkTrackingListControl.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CoffeeManagement.Views.CustomControls
+{
+    public partial class WorkTrackingListControl : UserControl
+    {
+        public WorkTrackingListControl()
+        {
+            InitializeComponent();
+        }
+    }
+}
