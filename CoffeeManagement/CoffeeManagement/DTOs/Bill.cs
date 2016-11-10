50a27c0866993c886931ead6295da8359a923280
Hao Svit <haonguyen.svit@gmail.com>
2016-11-10 23:18:14 +0700

    * Luu bill tam vao file

--- CoffeeManagement/CoffeeManagement/DTOs/Bill.cs
+++ CoffeeManagement/CoffeeManagement/DTOs/Bill.cs
@@ -3,31 +3,42 @@ using System.Collections;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
+using System.Runtime.Serialization;
 
 namespace CoffeeManagement.DTOs
 {
-    [Table("Bill")]
-    public class Bill
-    {
-        [Key]
-        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
-        public int Id { get; set; }
+	[DataContract]
+	[Table("Bill")]
+	public class Bill
+	{
+		[DataMember]
+		[Key]
+		[DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+		public int Id { get; set; }
 
-        public DateTime CreatedDateTime { get; set; }
-        public User CurrentUser { get; set; }
-	    public virtual ICollection<Item> Items { get; set; }
+		[DataMember]
+		public DateTime CreatedDateTime { get; set; }
+		[DataMember]
+		public User CurrentUser { get; set; }
+		[DataMember]
+		public virtual ICollection<Item> Items { get; set; }
 
+		[DataMember]
 		public virtual ICollection<Table> Tables { get; set; }
 
-        public int PreTotal { get; set; }
-        public bool IsClosed { get; set; }
-        public float DiscountRate { get; set; }
-        public int Total { get; set; }
+		[DataMember]
+		public int PreTotal { get; set; }
+		[DataMember]
+		public bool IsClosed { get; set; }
+		[DataMember]
+		public float DiscountRate { get; set; }
+		[DataMember]
+		public int Total { get; set; }
 
 		public Bill()
 		{
 			Items = new List<Item>();
 			Tables = new List<Table>();
 		}
-    }
+	}
 }
--- CoffeeManagement/CoffeeManagement/DTOs/Table.cs
+++ CoffeeManagement/CoffeeManagement/DTOs/Table.cs
@@ -1,18 +1,22 @@
 ﻿using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
+using System.Runtime.Serialization;
 
 namespace CoffeeManagement.DTOs
 {
-    [Table("Table")]
-    public class Table
-    {
-        [Key]
-        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
-        public int Id { get; set; }
-        public string Name { get; set; }
-	    public bool IsAvailable { get; set; }
+	[DataContract]
+	[Table("Table")]
+	public class Table
+	{
+		[DataMember]
+		[Key]
+		[DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+		public int Id { get; set; }
+		[DataMember]
+		public string Name { get; set; }
+		public bool IsAvailable { get; set; }
 
 		public virtual ICollection<Bill> Bills { get; set; }
-    }
+	}
 }
