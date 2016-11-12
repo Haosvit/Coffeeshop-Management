539c6c4f20dc4b981f0b75b0fe5d228e57b71c56
Hao Svit <haonguyen.svit@gmail.com>
2016-11-12 12:41:36 +0700

    clean up

--- CoffeeManagement/CoffeeManagement/Properties/Resources.Designer.cs
+++ CoffeeManagement/CoffeeManagement/Properties/Resources.Designer.cs
@@ -468,6 +468,16 @@ namespace CoffeeManagement.Properties {
         /// <summary>
         ///   Looks up a localized resource of type System.Drawing.Bitmap.
         /// </summary>
+        internal static System.Drawing.Bitmap splash1 {
+            get {
+                object obj = ResourceManager.GetObject("splash1", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
         internal static System.Drawing.Bitmap statistics {
             get {
                 object obj = ResourceManager.GetObject("statistics", resourceCulture);
--- CoffeeManagement/CoffeeManagement/Views/Splash.Designer.cs
+++ CoffeeManagement/CoffeeManagement/Views/Splash.Designer.cs
@@ -36,46 +36,51 @@
 			// 
 			// button1
 			// 
-			this.button1.FlatAppearance.BorderColor = System.Drawing.Color.White;
-			this.button1.FlatAppearance.BorderSize = 0;
+			this.button1.BackColor = System.Drawing.Color.Transparent;
+			this.button1.FlatAppearance.BorderColor = System.Drawing.Color.Brown;
 			this.button1.FlatAppearance.MouseDownBackColor = System.Drawing.Color.Azure;
+			this.button1.FlatAppearance.MouseOverBackColor = System.Drawing.Color.WhiteSmoke;
 			this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-			this.button1.Location = new System.Drawing.Point(506, 261);
+			this.button1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(64)))), ((int)(((byte)(0)))));
+			this.button1.Location = new System.Drawing.Point(496, 246);
 			this.button1.Name = "button1";
 			this.button1.Size = new System.Drawing.Size(82, 27);
 			this.button1.TabIndex = 0;
 			this.button1.Text = global::CoffeeManagement.Properties.Resources.Cancel;
-			this.button1.UseVisualStyleBackColor = true;
+			this.button1.UseVisualStyleBackColor = false;
 			this.button1.Click += new System.EventHandler(this.button1_Click);
 			// 
 			// _lbStatus
 			// 
 			this._lbStatus.AutoSize = true;
 			this._lbStatus.BackColor = System.Drawing.Color.Transparent;
-			this._lbStatus.Location = new System.Drawing.Point(12, 268);
+			this._lbStatus.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this._lbStatus.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(64)))), ((int)(((byte)(0)))));
+			this._lbStatus.Location = new System.Drawing.Point(37, 175);
 			this._lbStatus.Name = "_lbStatus";
-			this._lbStatus.Size = new System.Drawing.Size(0, 13);
+			this._lbStatus.Size = new System.Drawing.Size(56, 13);
 			this._lbStatus.TabIndex = 1;
+			this._lbStatus.Text = "Đang tải...";
 			// 
 			// _lbAppName
 			// 
 			this._lbAppName.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
 			this._lbAppName.BackColor = System.Drawing.Color.Transparent;
-			this._lbAppName.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-			this._lbAppName.ForeColor = System.Drawing.Color.WhiteSmoke;
-			this._lbAppName.Location = new System.Drawing.Point(1, 132);
+			this._lbAppName.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this._lbAppName.ForeColor = System.Drawing.Color.Maroon;
+			this._lbAppName.Location = new System.Drawing.Point(34, 139);
 			this._lbAppName.Name = "_lbAppName";
-			this._lbAppName.Size = new System.Drawing.Size(600, 24);
+			this._lbAppName.Size = new System.Drawing.Size(556, 36);
 			this._lbAppName.TabIndex = 2;
-			this._lbAppName.Text = global::CoffeeManagement.Properties.Resources.AppName;
+			this._lbAppName.Text = "Coffee Manager";
 			// 
 			// Splash
 			// 
 			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
 			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-			this.BackgroundImage = global::CoffeeManagement.Properties.Resources.splash;
-			this.ClientSize = new System.Drawing.Size(600, 300);
+			this.BackgroundImage = global::CoffeeManagement.Properties.Resources.splash1;
+			this.ClientSize = new System.Drawing.Size(590, 333);
 			this.Controls.Add(this._lbAppName);
 			this.Controls.Add(this._lbStatus);
 			this.Controls.Add(this.button1);
