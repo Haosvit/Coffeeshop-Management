2beedc1e96c6f9cefd588ac6e3333ddfdcd585e2
hao.svit <haonguyen.svit@gmail.com>
2018-06-16 11:57:09 +0700

    * Fixed local db * renamed in DbContext fluent Api

--- CoffeeManagement/CoffeeManagement/DAO/DataContext.cs
+++ CoffeeManagement/CoffeeManagement/DAO/DataContext.cs
@@ -6,7 +6,7 @@ namespace CoffeeManagement.DAO
 {
 	public class CoffeeDbContext : DbContext
 	{
-		private static string _dbName = "COFFEE_MANAGEMER_DB";
+		private static string _dbName = "COFFEE_MANAGEMENT_DB";
 
 		// DbSets
 		public DbSet<BasicSalary> BasicSalaries { get; set; }
@@ -16,9 +16,10 @@ namespace CoffeeManagement.DAO
 		public DbSet<Table> Tables { get; set; }
 		public DbSet<Unit> Units { get; set; }
 		public DbSet<User> Users { get; set; }
+
 		public CoffeeDbContext() : base(_dbName)
 		{
-		// switch on these lines to drop and create new database when model changes.
+			// switch on these lines to drop and create new database when model changes.
 			Database.SetInitializer(new DbInitializer());
 			Database.Initialize(false);
 			if (Database.CreateIfNotExists())
@@ -31,9 +32,9 @@ namespace CoffeeManagement.DAO
 		protected override void OnModelCreating(DbModelBuilder modelBuilder)
 		{
 			modelBuilder.Entity<Item>()
-				.HasRequired(m => m.Unit)
-				.WithMany(t => t.Items)
-				.HasForeignKey(m => m.UnitId);
+				.HasRequired(item => item.Unit)
+				.WithMany(unit => unit.Items)
+				.HasForeignKey(item => item.UnitId);
 
 			base.OnModelCreating(modelBuilder);
 		}
